61fad4d5a44f1f1a2d407f4d8d26789b1fcec34d
Umesh Singadi <umeshsingadi007@gmail.com>
2024-04-12 13:30:32 +0530

    add Error toast

--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -2,10 +2,12 @@ import { auth } from "../firebase/firebase";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { setDoc, doc } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
+import useShowToast from "./useShowToast";
 
 function useSignUpWithEmailAndPassword() {
   const [createUserWithEmailAndPassword, , loading, error] =
     useCreateUserWithEmailAndPassword(auth);
+  const showToast = useShowToast();
   const signup = async (inputs) => {
     if (
       !inputs.email ||
@@ -13,7 +15,7 @@ function useSignUpWithEmailAndPassword() {
       !inputs.fullName ||
       !inputs.userName
     ) {
-      console.log("Please fill all the fields");
+      showToast("Error", "Please fill all the fields", "error");
       return;
     }
     try {
@@ -22,15 +24,13 @@ function useSignUpWithEmailAndPassword() {
         inputs.password
       );
       if (!newUser && error) {
-        console.log(error);
+        showToast("Error", error.message, "error");
         return;
       }
-
       if (newUser) {
         const userDoc = {
           uid: newUser.user.uid,
           email: inputs.email,
-          password: inputs.password,
           userName: inputs.userName,
           fullName: inputs.fullName,
           bio: "",
@@ -45,7 +45,7 @@ function useSignUpWithEmailAndPassword() {
         localStorage.setItem("user-info", JSON.stringify(userDoc));
       }
     } catch (error) {
-      console.log(error);
+      showToast("Error", error.message, "error");
     }
   };
   return { loading, error, signup };
