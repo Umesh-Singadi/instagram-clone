192ea4c9083ad244dc05e286488c89d59bbf1de9
Umesh Singadi <umeshsingadi007@gmail.com>
2024-04-13 16:48:46 +0530

    create authStore with zustand

--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -3,11 +3,14 @@ import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { setDoc, doc } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 import useShowToast from "./useShowToast";
+import useAuthStore from "../store/authStore";
 
 function useSignUpWithEmailAndPassword() {
   const [createUserWithEmailAndPassword, , loading, error] =
     useCreateUserWithEmailAndPassword(auth);
   const showToast = useShowToast();
+  const loginUser = useAuthStore((state) => state.login);
+
   const signup = async (inputs) => {
     if (
       !inputs.email ||
@@ -43,6 +46,7 @@ function useSignUpWithEmailAndPassword() {
 
         await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
         localStorage.setItem("user-info", JSON.stringify(userDoc));
+        loginUser(userDoc);
       }
     } catch (error) {
       showToast("Error", error.message, "error");
