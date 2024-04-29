742887a335d902c76ed38419c9df3bc4aae6889e
Umesh Singadi <umeshsingadi007@gmail.com>
2024-04-29 15:57:33 +0530

    Connected design to the backend

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhbio6qztYDYxVCJby3zyFSh41QvBYnN8",
-  authDomain: "insta-clone-868a7.firebaseapp.com",
-  projectId: "insta-clone-868a7",
-  storageBucket: "insta-clone-868a7.appspot.com",
-  messagingSenderId: "147744002001",
-  appId: "1:147744002001:web:fffd5f58a6511229bddaf0",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
